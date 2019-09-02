6fa458c8f8b5bf10b2eceff81234cc5286a3e0e6
Alexey Semin <semin@mercurydevelopment.com>
2019-09-02 09:00:18 +0400

    Fixed response struct

--- golang-service/response/response-structs.go
+++ golang-service/response/response-structs.go
@@ -3,14 +3,12 @@ package response
 import "time"
 
 type Log struct {
-	Start    time.Time
-	End      time.Time
 	Duration float64
 }
 
 func NewLog(start time.Time, end time.Time) *Log {
 	duration := end.Sub(start).Seconds()
-	return &Log{start, end, duration}
+	return &Log{duration}
 }
 
 type MinMaxAvgRating struct {
